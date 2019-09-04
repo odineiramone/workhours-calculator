0aac9e23d267f0bbe37644af742b81ffe213b09f
Odinei Ramone <odineiramone@gmail.com>
2019-09-04 19:33:40 -0300

    Created tests for command line

---
+++ test/integration/calculator_test.rb
@@ -0,0 +1,11 @@
+require './test/test_helper'
+
+class CalculatorTest < Minitest::Test
+  def test_returns_formated_time
+    output, errors = capture_subprocess_io do
+      system "ruby calculator.rb -p '09:00 12:00 13:15 18:37 19:49 21:13'"
+    end
+
+    assert_equal "You worked for 9:46 in this period.\n", output
+  end
+end
