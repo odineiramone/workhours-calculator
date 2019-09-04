cec2c18aec7f92b0fc9edced05270976f41c1e44
Odinei Ramone <odineiramone@gmail.com>
2019-09-04 19:46:12 -0300

    add underline on unused variable

--- test/integration/calculator_test.rb
+++ test/integration/calculator_test.rb
@@ -2,7 +2,7 @@ require './test/test_helper'
 
 class CalculatorTest < Minitest::Test
   def test_returns_formated_time
-    output, errors = capture_subprocess_io do
+    output, _errors = capture_subprocess_io do
       system "ruby calculator.rb -p '09:00 12:00 13:15 18:37 19:49 21:13'"
     end
 
